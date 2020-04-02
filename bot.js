12149fcc9e8eb9a5c5ff0729bf7ed3720e913913
Cassie de Git <cassandra.m.degit@gmail.com>
2020-04-01 18:27:26 -0700

    Add alt formats

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ const initDoc = async () => {
     console.log('Connected to Google Sheets')
 }
 
-const findAndUpdatePrice = async (price, author, message) => {
+const findAndLoadSheet = async (author) => {
     let correctSheetIndex
 
     doc.sheetsByIndex.forEach((sheet, i) => {
@@ -35,30 +35,79 @@ const findAndUpdatePrice = async (price, author, message) => {
         }
     })
 
-    if (correctSheetIndex) {
-        const sheet = doc.sheetsByIndex[correctSheetIndex]
-        await sheet.loadCells('C6:C17')
+    const sheet = doc.sheetsByIndex[correctSheetIndex]
+    await sheet.loadCells('C6:C17')
 
-        // Determine which time slot you are in
-        // Get the day and time in PST from the message
-        const vanLocaleString = message.createdAt.toLocaleString('en-US', { timeZone: 'America/Vancouver'})
-        const vanDate = new Date(vanLocaleString)
+    return sheet
+}
 
-        // Day of week, 0 indexed
-        const dayOfWeek = vanDate.getDay() - 1
-        const hour = vanDate.getHours()
+const updatePrice = async (dayOfWeek, hour, price, sheet) => {
+    const cellIndex = 6 + (dayOfWeek * 2) + (hour < 12 ? 0 : 1)
+    const cell = sheet.getCellByA1(`C${cellIndex}`)
+    cell.value = price
+    await sheet.saveUpdatedCells()
+}
 
-        const cellIndex = 6 + (dayOfWeek * 2) + (hour < 12 ? 0 : 1)
+const getLocalizedDate = (createdAt) => {
+    const vanLocaleString = createdAt.toLocaleString('en-US', { timeZone: 'America/Vancouver'})
+    return new Date(vanLocaleString)
+}
 
-        const cell = sheet.getCellByA1(`C${cellIndex}`)
-        cell.value = price
-        await sheet.saveUpdatedCells()
+const getDayFromCreatedAt = (createdAt) => {
+    // Day of week, 0 indexed
+    return getLocalizedDate(createdAt).getDay() - 1
+}
 
-        message.react(hour < 12 ? '☀️' : '🌘')
+const getHourFromCreatedAt = (createdAt) => {
+    return getLocalizedDate(createdAt).getHours()
+}
+
+const checkContentForDay = (content) => {
+    if (/monday|mon/i.test(content)) {
+        return 0
+    } else if (/tuesday|tues/i.test(content)) {
+        return 1
+    } else if (/wednesday|wed/i.test(content)) {
+        return 2
+    } else if (/thursday|thur/i.test(content)) {
+        return 3
+    } else if (/friday|fri/i.test(content)) {
+        return 4
+    } else if (/saturday|sat/i.test(content)) {
+        return 5
     } else {
-        // Show an error message
-        message.channel.send('Sorry, something went wrong')
+        return -1
     }
+}
+
+const handleMessage = async (message) => {
+    // Extract the number
+    const match = message.content.match(/([0-9]+)\/t/i)
+    const price = match && parseInt(match[1])
+
+    // Get the name of the user
+    const author = authorMap[message.author.username]
+
+    // Does this message contain a time of day?
+    let hour
+    if (/morning/i.test(message.content)) {
+        hour = 1
+    } else if (/afternoon/i.test(message.content)) {
+        hour = 13
+    } else {
+        hour = getHourFromCreatedAt(message.createdAt)
+    }
+
+    // Does this message contain a weekday?
+    let dayOfWeek = checkContentForDay(message.content)
+    if (dayOfWeek === -1) {
+        dayOfWeek = getDayFromCreatedAt(message.createdAt)
+    }
+
+    const sheet = await findAndLoadSheet(author)
+    await updatePrice(dayOfWeek, hour, price, sheet)
+
+    message.react(hour < 12 ? '☀️' : '🌘')
 }
 
 client.on('ready', () => {
@@ -67,16 +116,12 @@ client.on('ready', () => {
  
 client.on('message', (message) => {
     // Does this message contain a turnip price?
-    // Expected format: <some number>/t
+    // Expected formats:
+    //     <some number>/t
+    //     <num>/t morning
+    //     <num>/t friday morning
     if (/[0-9]+\/t/i.test(message.content)) {
-        // Extract the number
-        const match = message.content.match(/([0-9]+)\/t/i)
-        const price = match && parseInt(match[1])
-
-        // Get the name of the user
-        const author = authorMap[message.author.username]
-
-        findAndUpdatePrice(price, author, message)
+        handleMessage(message)
     }
 })
 
